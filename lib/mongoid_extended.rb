c6b6e56a231ed5ace37f587a1efe11d7ee21e06c
Spirit <neverlandxy.naix@gmail.com>
2015-09-29 01:06:02 +0800

    add concern

--- lib/mongoid_extended.rb
+++ lib/mongoid_extended.rb
@@ -1,7 +1,22 @@
 # encoding: utf-8
 
-require 'mongoid_extended/version'
+unless defined?(ActiveSupport::Concern)
+  require_relative './concern'
+end
 
 module MongoidExtended
-
+  class << self
+    # configure method is used for select extend modules
+    # @example Use soft delete module and object id module
+    #   MongoidExtend.configure(:SoftDelete, :ObjectId)
+    def configure *module_names
+      module_names.each do |module_name|
+        if const_defined?(module_name)
+          module_name.send(:configured)
+        else
+          raise NameError, "uninitialized constant `#{module_name}` in MongoidExtended"
+        end
+      end
+    end
+  end
 end
