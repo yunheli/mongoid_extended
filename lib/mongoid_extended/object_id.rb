fc23ba45b99a0e96e6755ec5c731437ac207fdbf
Spirit <neverlandxy.naix@gmail.com>
2015-09-29 00:54:33 +0800

    add ObjectId Extended

--- lib/mongoid_extended/object_id.rb
+++ lib/mongoid_extended/object_id.rb
@@ -2,5 +2,29 @@
 
 module MongoidExtended
   module ObjectId
+    class << self
+      def configured
+        BSON::ObjectId.send(:prepend, HashRepresentation)
+      end
+    end
+
+    # rewrite methods `BSON::ObjectId#as_json` & `BSON::ObjectId#to_json`
+    # Return the object id as a JSON hash representation.
+    #
+    # @example Get the object id as JSON.
+    #   object_id.as_json
+    #
+    # @return [ String ] The object id as a JSON string.
+    #
+    # @since 2.0.0
+    module HashRepresentation
+      def to_json
+        to_s
+      end
+
+      def as_json
+        to_s
+      end
+    end
   end
 end
