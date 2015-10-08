e19fa2ad02e241a9fde950557dc1b84caa257968
Spirit <neverlandxy.naix@gmail.com>
2015-10-08 15:40:12 +0800

    add the reporter on the very first line

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'mongoid_extended'
 require 'minitest/autorun'
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 Mongoid.load!(File.expand_path('../mongoid.yml', __FILE__), :test)
 
